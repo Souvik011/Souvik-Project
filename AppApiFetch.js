9bd701897915ac5cf87d2828858d79c6730cdf7f
Souvik011 <116072850+Souvik011@users.noreply.github.com>
2023-03-22 01:13:59 +0530

    Task 9

--- AppApiFetch.js
+++ AppApiFetch.js
@@ -10,22 +10,21 @@ function AppApiFetch() {
       setIsLoading(true);
       setError(null);
       try {
-        const response = await fetch("https://react-project-ec784-default-rtdb.firebaseio.com/Movies.json");
+        const response = await fetch("https://swapi.dev/api/films/");
         if (!response.ok) {
           throw new Error("Something went wrong ....Retrying");
         }
         const data = await response.json();
-   const  loadMovies = []
-   for(let key in data){
-    loadMovies.push({
-      id:key,
-      title:data[key].title,
-      openingText: data[key].openingText,
-      releaseDate:data[key].releaseDate,
-    })
-   }
   
-        setMovies(loadMovies);
+        const transformdata = data.results.map((movieList) => {
+          return {
+            id: movieList.episode_id,
+            title: movieList.title,
+            releaseDate: movieList.release_date,
+            openingText: movieList.opening_crawl,
+          };
+        });
+        setMovies(transformdata);
       } catch (error) {
         setError(error.message);
       }
@@ -39,46 +38,24 @@ function AppApiFetch() {
     const retryingremovHandler = (event) => {
       setError(event.target.parentNode.remove());
     };
-     async function  onAddMOvie  (movieitem) {
-     const response = await fetch('https://react-project-ec784-default-rtdb.firebaseio.com/Movies.json',{
-        method:'POST',
-        body:JSON.stringify(movieitem),
-        headers:{
-          'Content-Type':'application/json'
-        }
-      })
-      const data = await response.json()
-      console.log(data)
-    }
-  
-    async  function  onDelete (id){
-       const response = await  fetch(`https://react-project-ec784-default-rtdb.firebaseio.com/Movies/${id}.json/`,{
-        method: 'DELETE',
-        // body:JSON.stringify(id)
-  
-       })
-       return response.json()
-    }
-
     return (
-        <React.Fragment>
-          <section>
-            <button onClick={ApiHandler}>Fetch Movies</button>
-          </section>
-          <section>
-            {!isLoading && <MoviesList  setMovies={setMovies}   onDelete={onDelete}   movies={movies} />}
-    
-            {!isLoading && error && <p>{error} </p>}
-    
-            {isLoading && <p>Loading...</p>}
-          </section>
-    
-          <div>
-            {" "}
-            {error && <button onClick={retryingremovHandler}>Cancel</button>}
-          </div>
-        </React.Fragment>
-      );
-    }
-    
-    export default AppApiFetch;
+      <React.Fragment>
+        <section>
+          <button onClick={ApiHandler}>Fetch Movies</button>
+        </section>
+        <section>
+          {!isLoading && <MoviesList movies={movies} />}
+  
+          {!isLoading && error && <p>{error} </p>}
+  
+          {isLoading && <p>Loading...</p>}
+        </section>
+  
+        <div>
+          {" "}
+          {error && <button onClick={retryingremovHandler}>Cancel</button>}
+        </div>
+      </React.Fragment>
+    );
+  }
+  export default AppApiFetch;
